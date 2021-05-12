b50a4f7e83ccdb1e68c8b758928f7dfd37c037cd
Keith L <k8t_loise16@yahoo.com>
2021-05-12 19:32:48 +1200

    Update Section/ImageWithText.php

--- app/src/Sections/ImageWithText.php
+++ app/src/Sections/ImageWithText.php
@@ -7,6 +7,9 @@ namespace {
     use SilverStripe\Forms\DropdownField;
     use SilverStripe\Forms\FieldList;
     use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+    use SilverStripe\Forms\ListboxField;
+    use SilverStripe\ORM\ArrayList;
+    use SilverStripe\View\ArrayData;
 
     class ImageWithText extends Section
     {
@@ -15,7 +18,8 @@ namespace {
         private static $db = [
             'Content' => 'HTMLText',
             'ContentPosition' => 'Varchar',
-            'ContentContainer'=> 'Varchar'
+            'ContentContainer'=> 'Varchar',
+            'ContentPaddings' => 'Varchar'
         ];
 
         private static $has_one = [
@@ -41,11 +45,27 @@ namespace {
             ));
             $fields->addFieldToTab('Root.Main', DropdownField::create('ContentContainer', 'Content container',
                 array(
-                    'container-fluid p-0' => 'Container fluid',
-                    'container p-0'       => 'Fix-width',
-                    'container-small p-0' => 'Container small'
+                    'container-fluid' => 'Container fluid',
+                    'container'       => 'Fix-width',
+                    'container-small' => 'Container small'
                 )
             )->setDescription('<b>Fix-width</b> container (its max-width changes at each breakpoint)</br><b>Container fluid</b> for a full width container, spanning the entire width of the viewport.</br><b>Container small</b> (max-width fixed at 575px)'));
+            $fields->addFieldToTab('Root.Main', ListboxField::create('ContentPaddings', 'Content Paddings',
+                Padding::get()->map('Class', 'Name')));
+        }
+
+        public function getReadableContentPaddings()
+        {
+            $output = new ArrayList();
+            $paddings = json_decode($this->ContentPaddings);
+            if ($paddings) {
+                foreach ($paddings as $padding) {
+                    $output->push(
+                        new ArrayData(array('Name' => $padding))
+                    );
+                }
+            }
+            return $output;
         }
     }
 }
